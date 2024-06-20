07412306064f95e9c55b1ee0c2b4763c2dc15657
Alejandro Gómez <alejandrogomez2339@gmail.com>
2024-06-20 17:33:35 -0500

    for cate review

--- src/app/organization-chart.ts
+++ src/app/organization-chart.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ImportsModule } from './imports';
 import { TreeNode } from 'primeng/api';
+
 @Component({
     selector: 'app-organization-chart',
     templateUrl: './organization-chart.html',
@@ -17,7 +18,7 @@ export class OrganizationChart {
             type: 'person',
             data: {
                 image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
-                name: 'Amy Elsner',
+                name: 'Jorge Estévez Abreu',
                 title: 'CEO'
             },
             children: [
@@ -25,36 +26,143 @@ export class OrganizationChart {
                     expanded: true,
                     type: 'person',
                     data: {
-                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/annafali.png',
-                        name: 'Anna Fali',
-                        title: 'CMO'
+                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                        name: 'Ma. Fernanda Estevez Gonzalez',
+                        title: 'DIRECTORA GENERAL'
                     },
                     children: [
                         {
-                            label: 'Sales'
+                            expanded: true,
+                            type: 'person',
+                            data: {
+                                image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                name: 'Ma. Fernanda Estevez Gonzalez',
+                                title: 'DIRECTORA GENERAL'
+                            },
                         },
                         {
-                            label: 'Marketing'
+                            expanded: true,
+                            type: 'person',
+                            data: {
+                                image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                name: 'Ma. Fernanda Estevez Gonzalez',
+                                title: 'DIRECTORA GENERAL'
+                            },
+                            children: [
+                                {
+                                    expanded: true,
+                                    type: 'person',
+                                    data: {
+                                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                        name: 'Nombre del nuevo nodo',
+                                        title: 'GERENTE DE ALMACÉN'
+                                    },
+                                    children: [
+                                        {
+                                            label: 'Sub-Almacenes',
+                                            expanded: true,
+                                            children: [
+                                                {
+                                                    expanded: true,
+                                                    type: 'person',
+                                                    data: {
+                                                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                                        name: 'Nombre del nuevo nodo',
+                                                        title: 'Coordinador de Sub-Almacenes'
+                                                    },
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            label: 'Almacén General'
+                                        },
+                                        {
+                                            label: 'almacen foraneo'
+                                        }
+                                    ]
+                                },
+                                {
+                                    expanded: true,
+                                    type: 'person',
+                                    data: {
+                                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                        name: 'Nombre del nuevo nodo',
+                                        title: 'Título del nuevo nodo'
+                                    },
+                                },
+                                {
+                                    expanded: true,
+                                    type: 'person',
+                                    data: {
+                                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                        name: 'Nombre del nuevo nodo',
+                                        title: 'Título del nuevo nodo'
+                                    },
+                                },
+                                {
+                                    expanded: true,
+                                    type: 'person',
+                                    data: {
+                                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                        name: 'Nombre del nuevo nodo',
+                                        title: 'Título del nuevo nodo'
+                                    },
+                                }
+                            ]
+                        },
+                        {
+                            expanded: true,
+                            type: 'person',
+                            data: {
+                                image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                                name: 'Ma. Fernanda Estevez Gonzalez',
+                                title: 'DIRECTORA GENERAL'
+                            },
                         }
                     ]
                 },
-                {
-                    expanded: true,
-                    type: 'person',
-                    data: {
-                        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/stephenshaw.png',
-                        name: 'Stephen Shaw',
-                        title: 'CTO'
-                    },
-                    children: [
-                        {
-                            label: 'Development'
-                        },
-                        {
-                            label: 'UI/UX Design'
-                        }
-                    ]
-                }
+                // {
+                //     expanded: true,
+                //     type: 'person',
+                //     styleClass: 'bg-teal-500 text-white',
+                //     data: {
+                //         image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                //         name: 'Nombre del nuevo nodo',
+                //         title: 'Título del nuevo nodo'
+                //     },
+                //     children: [
+                //         {
+                //             expanded: true,
+                //             type: 'person',
+                //             styleClass: 'bg-teal-500 text-white',
+                //             data: {
+                //                 image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                //                 name: 'Nombre del nuevo nodo',
+                //                 title: 'Título del nuevo nodo'
+                //             },
+                //         },
+                //         {
+                //             expanded: true,
+                //             type: 'person',
+                //             styleClass: 'bg-teal-500 text-white',
+                //             data: {
+                //                 image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                //                 name: 'Nombre del nuevo nodo',
+                //                 title: 'Título del nuevo nodo'
+                //             },
+                //         },
+                //         {
+                //             expanded: true,
+                //             type: 'person',
+                //             styleClass: 'bg-teal-500 text-white',
+                //             data: {
+                //                 image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+                //                 name: 'Nombre del nuevo nodo',
+                //                 title: 'Título del nuevo nodo'
+                //             },
+                //         }
+                //     ]
+                // }
             ]
         }
     ];
